82274254845894c3ff11060268eeb351a54097ff
nahsiloh <lishan1512@gmail.com>
2019-10-14 16:38:36 +0800

    changed the port number

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const app = require("./app");
-const port = 5000;
+const env = app.get("env");
+
+let port;
+if (env === "development") {
+  port = 5000;
+}
 
 app.listen(port, () => console.log(`listening on port ${port}`));
