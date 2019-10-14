89ca12836117dd009250b363ef302bc6725c7b9a
nahsiloh <lishan1512@gmail.com>
2019-10-14 17:22:19 +0800

    added owner schema

--- models/Owner.js
+++ models/Owner.js
@@ -1,15 +1,25 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 //defining the Schema and model
 //creating a schema
 const ownerSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
-  salutation: { type: String, enum: ["Mr", "Mrs", "Ms", "Mdm", "Miss"] }
+  // salutation: { type: String, enum: ["Mr", "Mrs", "Ms", "Mdm", "Miss"] }
+  userName: { type: String, required: true, index: true, unique: true },
+  password: { type: String, required: true, minlength: 8 }
+  //can add select:false which hide fields when querying documents
 });
 
-ownerSchema.virtual("fullName").get(function() {
-  return `${this.salutation} ${this.firstName} ${this.lastName}`;
+// ownerSchema.virtual("fullName").get(function() {
+//   return `${this.salutation} ${this.firstName} ${this.lastName}`;
+// });
+
+ownerSchema.pre("save", async function(next) {
+  const rounds = 10;
+  this.password = await bcrypt.hash(this.password, rounds);
+  next();
 });
 
 //creating a model
